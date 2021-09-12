0fbe159f21ab5edafd90d02f58377e70a08ac9ea
Kristian Morath <kristian.morath@bluewin.ch>
2021-09-12 10:43:56 +0200

    going foward

--- src/index.js
+++ src/index.js
@@ -9,13 +9,19 @@ function FancyBorder(props) {
   );
 }
 
-function WelcomeDialog() {
+function Dialog(props) {
   return (
     <FancyBorder color="blue">
-      <h1 className="Dialog-title">Welcome</h1>
-      <p className="Dialog-message">Thank you for visiting our spacecraft!</p>
+      <h1 className="Dialog-title">{props.title}</h1>
+      <p className="Dialog-message">{props.message}</p>
     </FancyBorder>
   );
+}
+
+function WelcomeDialog() {
+  return (
+    <Dialog title="Welcome" message="Thank you for visiting our spacecraft!" />
+    );
 }
 
 ReactDOM.render(
